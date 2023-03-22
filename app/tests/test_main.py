313b43e8b9973d24b40df0d0084d1f1c0d97c912
scarletkibou <111670539+scarletkibou@users.noreply.github.com>
2023-03-22 14:47:48 +0700

    Update test_main.py

--- app/tests/test_main.py
+++ app/tests/test_main.py
@@ -5,7 +5,15 @@ from app.main import app
 client = TestClient(app)
 
 
-def test_read_main():
-    response = client.get("/")
+def test_get_name_Theetach():
+    name= "Theetach"
+    url=f"/callname/{name}"
+    expected_result ={"hello" : name}
+    actual_result = client.get(url)
+    assert actual_result.status_code == 200
+    assert actual_result.json() == expected_result
+    
+def test_post_name_junior():
+    response = client.post("/callname")
     assert response.status_code == 200
-    assert response.json() == {"Hello": "World"}
+    assert response.json() == {"hello" : "junior"}
